a9516eb8313475cb999d671ebb84716026b901c3
ssol_ <sucuvu@gmail.com>
2020-09-23 01:16:00 +0900

    error:무한루프

--- 2670.php
+++ 2670.php
@@ -5,17 +5,21 @@ for($i=0 ; $i<$number ; $i++){
     fscanf(STDIN, "%f", $num_Arr[]);
 }
 
-$final_max = $num_Arr[0];
+$final_max = 0;
 
-for($j=1;$j<$number;$j++){
-    if($num_Arr[$j] * $num_Arr[$j-1] >= $num_Arr[$j]){
-        $num_Arr[$j] = $num_Arr[$j] * $num_Arr[$j-1];
+for($j=0;$j<$number;$j++){
+    $multi = 1;
+
+    for($k=$j;$k<$num_Arr;$k++){
+        $multi *= $num_Arr[$k];
+
+        if($final_max < $multi){
+            $final_max = $multi;
+        }else{
+            $final_max = $final_max;
+        }
     }
-    $final_max = max($num_Arr);
-
 }
 
 echo round($final_max,3);
-
-
 ?>
