7e6121686cb56aa71c9bb441e0db6db9526d35d0
ssol_ <sucuvu@gmail.com>
2020-09-24 19:57:36 +0900

    1213

--- 1213.php
+++ 1213.php
@@ -1,14 +1,10 @@
 <?php
-
-
 fscanf(STDIN, "%s", $word);
 $arr_word = str_split($word);
 natcasesort($arr_word); //문자열 오름차순 정렬
 //print_r($arr_word);
 
 $arr_word2 = array_count_values($arr_word); // 알파벳별 갯수
-//echo count($arr_word2);
-//print_r($arr_word2);
 $word_value = array_values($arr_word2); // value배열
 $word_key  = array_keys($arr_word2); // key 배열
 
@@ -20,30 +16,22 @@ for($i=0;$i<count($arr_word2);$i++){ //총 알파벳 갯수만큼 돌기
     if($word_value[$i] % 2 == 1){
         $hole = $hole +1;
         $mid_word = $word_key[$i];
-    //    echo $mid_word."::";
-
     }else{
         $mid_word= $mid_word;
-       // echo $mid_word."::";
     }
 
     for ($j = 0; $j < intval($word_value[$i] / 2); $j++) { //
-      //  echo intval($word_value[$i] / 2);
         $final_word .= $word_key[$i];
-
-        //echo $final_word . "/";
     }
-
 
     if($hole >=2){
         echo "I'm Sorry Hansoo";
         exit;
     }
 }
-//echo ">".$final_word.">".$mid_word;
 $revelse_word = strrev($final_word);
-//echo $final_word. "            ".$revelse_word;
 echo $final_word.$mid_word.strrev($final_word);
+
 ?>
 
 
