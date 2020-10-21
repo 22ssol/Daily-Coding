c9b51b6640aa490148fbc419fd3782a99be56eb2
ssol_ <sucuvu@gmail.com>
2020-10-21 16:00:39 +0900

    8741 - 틀렸다고 나옴

--- 8741.php
+++ 8741.php
@@ -1,26 +1,9 @@
 <?php
-fscanf(STDIN, "%d", $n);
+fscanf(STDIN, "%d", $k);
 
-$sum=0;
-$sum2=0;
+$sum = (pow(2,$k)-1) * pow(2,$k-1); // 0 ~ (2^k -1) 까지의 합 구하기
 
-for($i=1;$i<=$n;$i++) {
-    $sum = $sum + $i;
-}
-echo "1차 합계".$sum ." ///// ";
-$binary_num = decbin($sum); //1. 이진수로 변환
-echo "2진수 변환".$binary_num." ///// ";
-//strlen($binary_num); // 변환한 이진수가 몇 자리인지.
-$num = '1';
 
-for($i=0;$i<strlen($binary_num);$i++){
-    $num =$num*'2'; //2. 이진수의 길이를 구해서 그 길이만큼의 1이 들어간 이진수를 만든다.
-}
-echo "2차 이진수".$num ." ///// ";
-//echo "2진수 변환".bindec($num) ." ///// ";
-for($i=1;$i<$num;$i++) {
-    $sum2 = $sum2 + $i; //3. 2번의 이진수를 십진수로 변환해서 싹 더함
-}
-printf(decbin($sum2)); //이진수로 변환
-
+//printf("%s",decbin($sum)); //이진수로 변환한 결과값 출력
+echo (string)decbin($sum); // 문자로 다시 변환하여 출력함
 ?>
