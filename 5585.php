0b6d797e96a0767ea9545a52d022033e9dbb0fc0
ssol_ <sucuvu@gmail.com>
2020-10-22 18:57:08 +0900

    5585

---
+++ 5585.php
@@ -0,0 +1,15 @@
+<?php
+fscanf(STDIN, "%d", $n);
+$money = 1000 -$n;
+$change = [500,100,50,10,1];
+$result = 0;
+
+for($i=0;$i<count($change);$i++){
+    if($money >= $change[$i]){
+        $result = sprintf('%d', $money / $change[$i]) + $result;
+        $money = $money % $change[$i];
+    }
+
+}
+echo sprintf('%d', $result);
+?>
