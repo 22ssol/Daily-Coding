496e756f5feeb4a2bed49afd3f2d8c17b2f7678e
ssol_ <sucuvu@gmail.com>
2020-10-26 22:46:32 +0900

    1259

---
+++ 1259.php
@@ -0,0 +1,19 @@
+<?php
+
+$num = array();
+$i =0;
+while(1){
+    fscanf(STDIN, "%d", $num[]);
+    $i ++;
+
+
+    /*
+    if(isset($num[$i]) == "0"){
+        break;
+    }
+    */
+}
+
+print_r($num);
+
+?>
