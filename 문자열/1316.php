0909f31bb4372b73fca9e3203f0d39a0f977dd82
ssol_ <sucuvu@gmail.com>
2020-10-28 18:04:14 +0900

    1316 - solving

--- 문자열/1316.php
+++ 문자열/1316.php
@@ -1,1 +1,21 @@
 <?php
+fscanf(STDIN, "%d", $n);
+
+$str = array();
+$sum = 0;
+$group = array();
+
+for($i=0;$i<$n;$i++){
+    fscanf(STDIN, "%s", $str[]);
+}
+
+for($i=0;$i<$n;$i++) {
+    $temp = array();
+    for($j=0;$j<strlen($str[$i]);$j++) {
+        $temp[$j] = substr($str[$i],$j,1);
+    }
+    array_push($group,$temp);
+}
+print_r($group);
+
+?>
