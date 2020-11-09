8de09a1dca88245034f8d9118ba45531ce50e70e
ssol_ <sucuvu@gmail.com>
2020-11-09 17:51:33 +0900

    1010

---
+++ 알고리즘/DP/1010.php
@@ -0,0 +1,21 @@
+<?php
+fscanf(STDIN, "%d", $t);
+$n = array();
+$m = array();
+
+for($i=0;$i<$t;$i++){
+    fscanf(STDIN, "%d %d", $n[], $m[]);
+}
+
+for($j=0;$j<$t;$j++){
+   echo fac($m[$j]) / fac($n[$j]) * fac($m[$j] - $n[$j])."\n";
+
+}
+function fac($n){
+    $sum = 1;
+    for($i=1;$i<=$n;$i++){
+        $sum = $sum * $i;
+    }
+    return $sum;
+}
+?>
