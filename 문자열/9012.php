e36936533bf71427f4b42a816a215dd322fb9b10
ssol_ <sucuvu@gmail.com>
2020-11-11 14:52:13 +0900

    9012

--- 문자열/9012.php
+++ 문자열/9012.php
@@ -5,6 +5,29 @@ $ps = array();
 for($i=0;$i<$t;$i++){
     fscanf(STDIN, "%s", $ps[]);
 }
+for($i=0;$i<$t;$i++){
+    $dap = 0;
+    for($j=0;$j<strlen($ps[$i]);$j++){
+        $str = substr($ps[$i], $j, 1);
+        if($str == "("){
+            $dap = $dap + 1;
+        }else if($str == ")"){
+            $dap = $dap - 1;
 
-print_r($ps);
+        }
+
+        if($dap < 0 ){
+            break;
+        }
+
+    }
+
+    if($dap == 0){
+        echo "YES\n";
+    }else{
+        echo "NO\n";
+    }
+
+}
+
 ?>
